8e635846fd05af21f3375307f82aa25ca6587afb
Gpravalika2002 <128896984+pravalikagodugu@users.noreply.github.com>
2025-06-01 22:42:23 +0530

    Initial commit

---
+++ gallery/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='products/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    def edit(self, name, description, image):
+        self.name = name
+        self.description = description
+        self.image = image
+        self.save()
+
+    def short_description(self):
+        words = self.description.split()
+        return ' '.join(words[:30]) + '...' if len(words) > 50 else self.description
---
+++ gallery/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+from .models import Product
+from .forms import ProductForm
+
+def home(request):
+    return HttpResponse('Hello, World!')
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'myapp/index.html', {'products': products})
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'myapp/index2.html', {'product': product})
+
+def edit_product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    else:
+        form = ProductForm(instance=product)
+    return render(request, 'myapp/edit.html', {'form': form})
+
+def delete_product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        product.delete()
+        return redirect('product_list')
+    return render(request, 'myapp/delete.html', {'product': product})
+
+# Create your views here.
